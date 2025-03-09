1e70b224141fcd3867084d09476bca560df8f4eb
RAYHAN HOSSAIN <22010701003@ipe.butex.edu.bd>
2025-03-09 22:51:02 +0600

    Add files via upload

---
+++ comment.py
@@ -0,0 +1,11 @@
+# comment by   (ctrl + forward slash + #) for multiple line comment at a single  click  
+# print("hello world")
+# print("hello world")
+# print("hello world")
+# print("hello world")
+# print("hello world")
+# print("hello world")
+# print("hello world")
+
+# comment by single line by # 
+
