562fc4e347bd574d5b493d8a4a0d0be4771d2f49
RAYHAN HOSSAIN <22010701003@ipe.butex.edu.bd>
2025-03-17 01:14:03 +0600

    Add files via upload

---
+++ input variable.py
@@ -0,0 +1,19 @@
+Name=input("enter your name:")
+print("your name",Name)
+
+value=input("enter some value:")
+print(type(value),value) # if we enter 25 program shows class str and 25 
+ 
+# if we want to change data type 
+ 
+# int(input("enter value:"))
+# float(input ("enter value:"))
+
+name= input("enter name:")
+age= int(input("enter age:"))
+marks= float(input("enter marks:"))
+
+print("welcome",name)
+print("age",age)
+print("marks",marks) 
+
